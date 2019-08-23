e7c4ecc98257438839d529a0f0f3ddaf719d354a
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-08-23 14:23:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,19 @@
-class Person  
+class Person
   
-    def name=(persons_name)
-    @this_persons_name = persons_name
+  def name=(name)
+    @name = name
   end
- 
+  
   def name
-    @this_persons_name
+    @name
   end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
 end
-
-
-Jon = Person.new
-Person.name = "Jon"
- 
-puts Person.name
-
-Jon.job = "Cop"
-end
-
